0cc5f9a2976912aef1c1d04ec77a1160e5f657f1
Mikael DEROME <mderome@e1r8p12.clusters.42paris.fr>
2022-10-15 12:55:37 +0200

    need test2

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -3,12 +3,12 @@
 FILE=/var/lib/mysql/.db_create
 if  [ ! -f "$FILE" ]
 then
-	echo "Je creer la BDD"
+	echo "Je creer la Base de donnee"
 	envsubst < /var/init.sql > /var/init_env.sql
 	service mysql start
 	mysql -D mysql < /var/init_env.sql | true
 	touch /var/lib/mysql/.db_create
 	service mysql stop | echo -n ""
 fi
-echo "BDD deja creer"
+echo "Base de donnee deja creer"
 exec mysqld_safe
--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,5 +1,5 @@
 
-FILE=/var/www/html/.exist
+FILE=/var/www/html/.config_create
 
 if  [ ! -f "$FILE" ]; then
 	echo "Create config"
@@ -7,7 +7,7 @@ if  [ ! -f "$FILE" ]; then
 	wp config create --dbname=$WP_DB --dbuser="wordpress" --dbpass=$PASSWORD --dbhost="mariadb" --path="/var/www/html/" --allow-root --skip-check
 	wp core install --url="localhost" --title="mderome site" --admin_user="mderome" --admin_password="mderome.2014" --admin_email="mderome@student.42.fr" --path="/var/www/html/" --allow-root
 	wp user create draicus draicus@student.42.fr --role=author --user_pass="qwerty12" --allow-root
-	touch /var/www/html/.exist
+	touch /var/www/html/.config_create
 fi
 echo "Config already done"
 exec php-fpm7.3 --nodaemonize
