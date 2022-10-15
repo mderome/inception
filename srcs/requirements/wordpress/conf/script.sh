67072754b207647a7776b91464c00b9e633cf327
Mikael DEROME <mderome@e1r8p12.clusters.42paris.fr>
2022-10-15 15:34:28 +0200

    need test6

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -3,9 +3,10 @@ FILE=/var/www/html/.config_create
 
 if  [ ! -f "$FILE" ]; then
 	echo "Create config"
-	mv /var/www/html/wp-config.php /var/www/html/test.php
-	echo "test1"
-	wp config create --path="/var/www/html/" --dbname=$WP_DB --dbuser="wordpress" --dbpass=$PASSWORD --dbhost="mariadb" --allow-root --skip-check
+	if[ -f /var/www/html/wp-config.php ]; then
+		rm /var/www/html/wp-config.php
+	fi
+	wp config create --path="/var/www/html/" --dbname=$WP_DB --dbuser=$MARIADB_USER --dbpass=$PASSWORD --dbhost=$MARIADB_USER_PWD --allow-root --skip-check
 	echo "wp config create reussi"
 	wp core install --url="localhost" --title="mderome site" --admin_user="mderome" --admin_password="mderome.2014" --admin_email="mderome@student.42.fr" --path="/var/www/html/" --allow-root
 	wp user create draicus draicus@student.42.fr --role=author --user_pass="qwerty12" --allow-root
